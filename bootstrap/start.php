44b3e314cd9ec37e5f415db1785273579f9f47b4
Luke <tanprasitp@hotmail.co.uk>
2014-11-11 15:59:00 +0000

    Added production url to start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('Lukes-MacBook-Pro-2.local'),
+	'production' => array('54.171.156.164')
 
 ));
 
