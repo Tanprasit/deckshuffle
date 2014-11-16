98d24df369f12f3b19071da3488da72b7adfb9f8
Luke <tanprasitp@hotmail.co.uk>
2014-11-16 16:44:07 +0000

    Moved analytics script into header

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -4,6 +4,16 @@
 	<meta charset="utf-8">
 	<title>Deck Citadel</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css' )}}">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-56765205-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <body>
 <nav class="navbar navbar-default" role="navigation">
@@ -82,15 +92,5 @@
 <script type='text/javascript' src="{{URL::asset('js/jquery.autosize.js')}}"></script> 
 <script type="text/javascript" src="{{URL::asset('js/validation.js')}}"></script>
 <script type="text/javascript" src="{{URL::asset('js/minisearch.js')}}"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-56765205-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 @yield('scripts')
 </html>
